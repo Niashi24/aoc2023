9322550810dcc6b18f6b8521440b1b1b374e098c
Leoshe22 <50159066+Niashi24@users.noreply.github.com>
2023-11-30 23:54:13 -0500

    Fill out template for day 1

---
+++ src/day1.rs
@@ -0,0 +1,20 @@
+﻿use crate::day::Day;
+pub struct Day1;
+
+pub struct Info {
+    
+}
+
+impl Day<Info> for Day1 {
+    fn parse_file(&self, file_content: String) -> Info {
+        todo!()
+    }
+
+    fn part_1(&self, data: &Info) -> i64 {
+        todo!()
+    }
+
+    fn part_2(&self, data: &Info) -> i64 {
+        todo!()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
+use crate::day1::Day1;
+use crate::day::run_with_test;
+
 mod day;
+mod day1;
 
 fn main() {
     println!("Hello, world!");
     
     
+}
+
+fn test_day1() -> std::io::Result<()> {
+    println!("<--------    Running Day 1    -------->");
+    run_with_test(
+        &Day1,
+        "input/day1e.txt",
+        (0,0),
+        "input/day1.txt"
+    )
 }
